702fe30cc3a1fca8cbd5fd968028425976951b5c
Medrodi <144428777+Medrodi@users.noreply.github.com>
2023-10-25 23:29:11 +0300

    Update Task_1.cpp

--- Home_Tasks/Task_1.cpp
+++ Home_Tasks/Task_1.cpp
@@ -1,19 +1,13 @@
 #include <iostream>
 
-int main() {
+int main(){
     int number;
-
-    // Запрос у пользователя ввести число для таблицы умножения
-    std::cout << "Введите число для таблицы умножения: ";
+    std::cout << " Введите число: ";
     std::cin >> number;
-
-    // Вывод заголовка таблицы
-    std::cout << "Таблица умножения для " << number << ":\n";
-
-    // Вывод таблицы умножения
-    for (int i = 1; i <= 10; i++) {
-        std::cout << number << " * " << i << " = " << (number * i) << std::endl;
+    
+    for (int i = 1; i <= 10; i++){
+        std::cout << i << " * " << number << '=' << i * number << std::endl;
+        
     }
-
     return 0;
 }
