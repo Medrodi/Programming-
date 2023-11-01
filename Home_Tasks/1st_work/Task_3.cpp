1dec8ca0b3253cde76cf479d2e52be9b82af1cd4
Medrodi <144428777+Medrodi@users.noreply.github.com>
2023-11-01 21:25:30 +0300

    Create Task_3.cpp

---
+++ Home_Tasks/1st_work/Task_3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   int x, y;
+    std::cout << "введите 2 числа"  << std::endl;
+    std::cin >> x >> y;
+    std::cout << x * y << std::endl;
+    return 0;
+}
